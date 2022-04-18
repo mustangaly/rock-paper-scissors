2f79dad07f437677feb32d1a929d9901450b3fc5
mustangaly <mustangaly@gmail.com>
2022-04-18 16:48:53 -0400

    Add ScoreBoard, Add UpdateScore() function
    Add a score board to display the real-time score of RPS to player.
    This was created to begin to move away from the alert display for
    the results of each round. Photos were scouted and saved for future
    display of the player's weapon of choice in a png format.

---
+++ README.md
@@ -0,0 +1,5 @@
+**README**
+
+The cover photo for this website was taken from [TimeKids] (https://www.timeforkids.com/k1/rock-paper-scissors/). 
+
+The individual photos of the rock, paper, and scissor icons are taken from [1000AwesomeThings] (http://1000awesomethings.com/2008/08/21/95-using-rock-paper-scissors-to-settle-anything/).
--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 let playerScore = 0;
 let computerScore = 0;
 
+let playerScoreDiv = document.querySelector('#playerScoreDiv');
+let computerScoreDiv = document.querySelector('#computerScoreDiv');
+let playerScoreDisplay = document.querySelector('#playerScoreDisplay');
+let computerScoreDisplay = document.querySelector('#computerScoreDisplay');
+
+
+function updateScore() {
+    playerScoreDisplay.textContent = `Player: ${playerScore}`;
+    computerScoreDisplay.textContent = `Computer: ${computerScore}`;
+    console.log("Player" + playerScore);
+    console.log("Computer" + computerScore);
+}
+
 function computerPlay() {
     let num = Math.floor(Math.random() *3); //returns a num: 0,1, or 2
     
@@ -19,42 +32,51 @@ function capitalize(string) {
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = capitalize(playerSelection);
+
     
     if (playerSelection == "Rock" && computerSelection == "Rock"){
+        updateScore();
         return "It's a tie! You both chose 'Rock.' The score is player: " + playerScore + " vs. the computer: " + computerScore + ".";
     }
     else if (playerSelection == "Rock" && computerSelection == "Paper") {
         computerScore++;
+        updateScore();
         return "You Lose! Paper beats Rock. The score is player: " + playerScore + " vs. the computer: " + computerScore + ".";
     }
     else if (playerSelection == "Rock" && computerSelection == "Scissors"){
         playerScore++;
+        updateScore();
         return "You Win! Rock beats Scissors. The score is player: " + playerScore + " vs. the computer: " + computerScore + ".";
     }
     else if (playerSelection == "Paper" && computerSelection == "Rock"){
         playerScore++;
+        updateScore();
         return "You Win! Paper beats Rock. The score is player: " + playerScore + " vs. the computer: " + computerScore + ".";
     }
     else if (playerSelection == "Paper" && computerSelection == "Paper"){
+        updateScore();
         return "It's a tie! You both chose 'Paper.' The score is player: " + playerScore + " vs. the computer: " + computerScore + ".";
     }
     else if (playerSelection == "Paper" && computerSelection == "Scissors"){
         computerScore++;
+        updateScore();
         return "You Lose! Scissors beats Paper. The score is player: " + playerScore + " vs. the computer: " + computerScore + ".";
     }
     else if (playerSelection == "Scissors" && computerSelection == "Rock"){
         computerScore++;
+        updateScore();
         return "You Lose! Rock beats Scissors. The score is player: " + playerScore + " vs. the computer: " + computerScore + ".";
     }
     else if (playerSelection == "Scissors" && computerSelection == "Paper"){
         playerScore++;
+        updateScore();
         return "You Win! Scissors beats Paper. The score is player: " + playerScore + " vs. the computer: " + computerScore + ".";
     }
     else if (playerSelection == "Scissors" && computerSelection == "Scissors"){
+        updateScore();
         return "It's a tie! You both chose 'Scissors.' The score is player: " + playerScore + " vs. the computer: " + computerScore + ".";
     }
     else return "Are you sure you entered 'Rock,' 'Paper,' or 'Scissors?'";
-
 }
 
 //function game() {
@@ -88,3 +110,5 @@ pButton.addEventListener('click', () => {
 sButton.addEventListener('click', () => {
     alert(playRound("Scissors", computerPlay()));
 });
+
+
