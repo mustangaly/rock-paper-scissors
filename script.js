0f5e052a7d8f6d69f54d8c7db2c9cf791665c024
mustangaly <mustangaly@gmail.com>
2022-04-18 17:15:56 -0400

    Add resultsDiv, Eliminate alerts
    Add a resultsDiv to portray an interactive UI without the alert popups.
    This allows for better interactivity.

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 let playerScore = 0;
 let computerScore = 0;
 
+let resultsDiv = document.querySelector('.resultsDiv');
 let playerScoreDiv = document.querySelector('#playerScoreDiv');
 let computerScoreDiv = document.querySelector('#computerScoreDiv');
 let playerScoreDisplay = document.querySelector('#playerScoreDisplay');
 let computerScoreDisplay = document.querySelector('#computerScoreDisplay');
 
+let resultsDisplay = document.querySelector('.roundResults');
 
 function updateScore() {
     playerScoreDisplay.textContent = `Player: ${playerScore}`;
     computerScoreDisplay.textContent = `Computer: ${computerScore}`;
-    console.log("Player" + playerScore);
-    console.log("Computer" + computerScore);
 }
+
 
 function computerPlay() {
     let num = Math.floor(Math.random() *3); //returns a num: 0,1, or 2
@@ -36,79 +37,63 @@ function playRound(playerSelection, computerSelection) {
     
     if (playerSelection == "Rock" && computerSelection == "Rock"){
         updateScore();
-        return "It's a tie! You both chose 'Rock.' The score is player: " + playerScore + " vs. the computer: " + computerScore + ".";
+        resultsDisplay.textContent = "It's a tie! You both chose Rock.";
     }
     else if (playerSelection == "Rock" && computerSelection == "Paper") {
         computerScore++;
         updateScore();
-        return "You Lose! Paper beats Rock. The score is player: " + playerScore + " vs. the computer: " + computerScore + ".";
+        resultsDisplay.textContent = "You Lose! Paper beats Rock."; 
     }
     else if (playerSelection == "Rock" && computerSelection == "Scissors"){
         playerScore++;
         updateScore();
-        return "You Win! Rock beats Scissors. The score is player: " + playerScore + " vs. the computer: " + computerScore + ".";
+        resultsDisplay.textContent =  "You Win! Rock beats Scissors.";
     }
     else if (playerSelection == "Paper" && computerSelection == "Rock"){
         playerScore++;
         updateScore();
-        return "You Win! Paper beats Rock. The score is player: " + playerScore + " vs. the computer: " + computerScore + ".";
+        resultsDisplay.textContent =  "You Win! Paper beats Rock.";
     }
     else if (playerSelection == "Paper" && computerSelection == "Paper"){
         updateScore();
-        return "It's a tie! You both chose 'Paper.' The score is player: " + playerScore + " vs. the computer: " + computerScore + ".";
+        resultsDisplay.textContent =  "It's a tie! You both chose Paper.";
     }
     else if (playerSelection == "Paper" && computerSelection == "Scissors"){
         computerScore++;
         updateScore();
-        return "You Lose! Scissors beats Paper. The score is player: " + playerScore + " vs. the computer: " + computerScore + ".";
+        resultsDisplay.textContent =  "You Lose! Scissors beats Paper.";
     }
     else if (playerSelection == "Scissors" && computerSelection == "Rock"){
         computerScore++;
         updateScore();
-        return "You Lose! Rock beats Scissors. The score is player: " + playerScore + " vs. the computer: " + computerScore + ".";
+        resultsDisplay.textContent =  "You Lose! Rock beats Scissors.";
     }
     else if (playerSelection == "Scissors" && computerSelection == "Paper"){
         playerScore++;
         updateScore();
-        return "You Win! Scissors beats Paper. The score is player: " + playerScore + " vs. the computer: " + computerScore + ".";
+        resultsDisplay.textContent =  "You Win! Scissors beats Paper.";
     }
     else if (playerSelection == "Scissors" && computerSelection == "Scissors"){
         updateScore();
-        return "It's a tie! You both chose 'Scissors.' The score is player: " + playerScore + " vs. the computer: " + computerScore + ".";
+        resultsDisplay.textContent =  "It's a tie! You both chose Scissors.";
     }
     else return "Are you sure you entered 'Rock,' 'Paper,' or 'Scissors?'";
 }
-
-//function game() {
-
- //   while (playerScore < 5 && computerScore < 5){
- //       console.log(playRound(prompt("Do you want to play 'Rock,' 'Paper,' or 'Scissors?'"), computerPlay())); 
- //   }
-
-  //  if (playerScore === 5){
-  //      return "You won the game! The score was player: " + playerScore + " vs. the computer: " + computerScore + ".";
-  //  }
-  //  else if (computerScore === 5){
-  //      return "You lost the game. The computer won. The score was player: " + playerScore + " vs. the computer: " + computerScore + ".";
-  //  }
-   // else return "error";
-
-//}
 
 const rButton = document.querySelector('#rockButton');
 const pButton = document.querySelector('#paperButton');
 const sButton = document.querySelector('#scissorsButton');
 
 rButton.addEventListener('click', () => {
-    alert(playRound("Rock", computerPlay()));
+    playRound("Rock", computerPlay());
 });
 
 pButton.addEventListener('click', () => {
-    alert(playRound("Paper", computerPlay()));
+    playRound("Paper", computerPlay());
 });
 
 sButton.addEventListener('click', () => {
-    alert(playRound("Scissors", computerPlay()));
+    playRound("Scissors", computerPlay());
 });
 
 
